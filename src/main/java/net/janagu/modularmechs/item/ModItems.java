d15310a2fbd6792e8526a4abb891c3c8fb821aa3
JaNaGu44 <106625561+JaNaGu44@users.noreply.github.com>
2023-02-27 23:29:16 +0100

    added vent cover for future crafting recepies (fix)

--- src/main/java/net/janagu/modularmechs/item/ModItems.java
+++ src/main/java/net/janagu/modularmechs/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.janagu.modularmechs.item;
 
 import net.janagu.modularmechs.Modularmechs;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
